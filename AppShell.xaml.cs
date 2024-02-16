2194d5f49f82cb5c4b4ac2c8cade1a1d97f6698f
blakepulman <132303516+blakepulman@users.noreply.github.com>
2024-02-16 14:14:34 +1300

    Add files via upload

---
+++ AppShell.xaml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace MauiIosEditorScrollingBugDemo;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MauiIosEditorScrollingBugDemo;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
